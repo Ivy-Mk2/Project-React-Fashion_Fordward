8fce907c4aab01f2c61442aee7b8d213297bb564
Ivy-Mk2 <119135234+Ivy-Mk2@users.noreply.github.com>
2026-03-21 13:37:43 -0500

    feat: implement phase 2 state, persistence, services and hooks

--- react-app/src/pages/Productos.tsx
+++ react-app/src/pages/Productos.tsx
@@ -1,20 +1,48 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
-
+import { useProducts } from '../hooks/useProducts';
 
 const Productos: React.FC = () => {
-    return (
-        <div>
-            <Header/>
-                <div className='productos'>
-                    <div className=''>
-                        div
-                    </div>
-                </div>
-            <Footer/>
+  const { products } = useProducts();
+  return (
+    <div>
+      <Header />
+      <section className="featured">
+        <div className="featured__header">
+          <h3 className="featured__title">Productos</h3>
         </div>
-    );
+        <div className="featured__display">
+          {products.map((product) => (
+            <div className="product-card" key={product.id}>
+              <div className="product-card__img-container">
+                <img
+                  src={product.primaryImg}
+                  alt={product.description}
+                  className="product-card__img product-card__img--primary"
+                />
+                <img
+                  src={product.secondaryImg}
+                  alt={product.description}
+                  className="product-card__img product-card__img--secondary"
+                />
+              </div>
+              <div className="product-card__description">
+                <Link className="product-card__description" to="/product_detail">
+                  {product.name}
+                </Link>
+              </div>
+              <div className="product-card__price">
+                <span className="product-card__price--original">${product.originalPrice.toFixed(2)}</span>
+                <span className="product-card__price--discounted">${product.discountedPrice.toFixed(2)}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+      <Footer />
+    </div>
+  );
 };
 
 export default Productos;
